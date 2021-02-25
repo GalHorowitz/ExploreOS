1ddcf8ed3f70acdbb1a2a96e8d7b8b40785a3be2
Gal Horowitz <galush.horowitz@gmail.com>
2021-02-25 21:27:01 +0200

    Debugging clean-up

--- build_and_debug.sh
+++ build_and_debug.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cargo run
+cargo run kernel_debug
 
 qemu-system-i386 -serial stdio -drive format=raw,file=build/new_os.img -s -S
--- debug_kernel.sh
+++ debug_kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 gdb-multiarch -ex "set disassembly-flavor intel" -ex "set arch i386" \
- 	-ex "file build/kernel/i586-unknown-linux-gnu/release/kernel" \
+ 	-ex "file build/kernel/i586-unknown-linux-gnu/debug/kernel" \
 	-ex "target remote localhost:1234" -ex "set disassemble-next-line on" \
 	-ex "show disassemble-next-line" -ex "b entry" -ex "c"
